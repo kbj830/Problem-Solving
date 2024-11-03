1fa8b9fc3d560b2f668a002dd4b4703f9daa54d8
Baekjun Kang <112003528+kbj830@users.noreply.github.com>
2024-11-03 22:08:07 +0900

    Create base_code.cpp

---
+++ base_code.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+	// Hello World!
+}
+
+int main() {
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	solve();
+	return 0;
+}
