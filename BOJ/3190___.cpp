22e4ae25627f93fa518d556ca5cca87aad98df98
Baekjun Kang <kbj830@naver.com>
2024-11-19 22:31:13 +0900

    solved boj 3190

--- BOJ/3190___.cpp
+++ BOJ/3190___.cpp
@@ -3,91 +3,76 @@
 
 using namespace std;
 
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+
+int n, k, l, x, y, dir = 0, ans = 0;
 int board[100][100];
-
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
-
-int n, k, l, dir = 0, ans = 0, x, y;
 queue<pair<int, int>> snake;
+queue<pair<int, char>> rotations;
 
-bool inRange(int r, int c, int n) {
-    return r >= 0 && r < n && c >= 0 && c < n;
+bool inRange(int a, int b) {
+    return a >= 0 && a < n && b >= 0 && b < n;
+}
+
+void solve() {
+    cin >> n >> k;
+    for (int i = 0; i < k; i++) {
+        int tmp1, tmp2;
+        cin >> tmp1 >> tmp2;
+        board[tmp1 - 1][tmp2 - 1] = 1;
+    }
+    
+    x = 0, y = 0;
+    snake.push({0, 0});
+    board[0][0] = 2;
+
+    cin >> l;
+    for (int i = 0; i < l; i++) {
+        int t; char rot;
+        cin >> t >> rot;
+        rotations.push({t, rot});
+    }
+
+    while (true) {
+        ans++;
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+
+        if (!inRange(nx, ny) || board[nx][ny] == 2) {
+            cout << ans << '\n';
+            return;
+        }
+        
+        if (board[nx][ny] == 1) {
+            board[nx][ny] = 2;
+            snake.push({nx, ny});
+        } else {
+            board[nx][ny] = 2;
+            snake.push({nx, ny});
+            int tail_x = snake.front().first;
+            int tail_y = snake.front().second;
+            snake.pop();
+            board[tail_x][tail_y] = 0;
+        }
+        
+        x = nx;
+        y = ny;
+
+        if (!rotations.empty() && rotations.front().first == ans) {
+            char rot = rotations.front().second;
+            rotations.pop();
+            if (rot == 'D') {
+                dir = (dir + 1) % 4;
+            } else {
+                dir = (dir + 3) % 4;
+            }
+        }
+    }
 }
 
 int main() {
-    cin >> n >> k;
-    for (int i = 0; i < k; i++) {
-        int r, c;
-        cin >> r >> c;
-        board[r - 1][c - 1] = -1;
-    }
-    
-    cin >> l;
-    int now = 0;
-    snake.push({0, 0});
-    board[0][0] = -1;
-    int headR = 0, headC = 0;
-    for (int i = 0; i < l; i++) {
-        int x;
-        char c;
-        cin >> x >> c;
-        while (now < x) {
-            headR += dx[dir];
-            headC += dy[dir];
-            now++;
-            
-            if (!inRange(headR, headC, n)) {
-                cout << now << endl;
-                return 0;
-            }
-            snake.push({headR, headC});
-            if (board[headR][headC] == 1) {
-                cout << now << endl;
-                return 0;
-            }
-            if (board[headR][headC] == -1) {
-                board[headR][headC] = 1;
-                continue;
-            }
-            
-            board[headR][headC] = 1;
-            int tailR = snake.front().first;
-            int tailC = snake.front().second;
-            snake.pop();
-            board[tailR][tailC] = 0;
-        }
-        if (c == 'L') {
-            dir = (dir + 3) % 4;
-        }
-        if (c == 'D') {
-            dir = (dir + 1) % 4;
-        }
-    }
-    while (true) {
-        headR += dx[dir];
-        headC += dy[dir];
-        now++;
-        
-        if (!inRange(headR, headC, n)) {
-            break;
-        }
-        snake.push({headR, headC});
-        if (board[headR][headC] == 1) {
-            break;
-        }
-        if (board[headR][headC] == -1) {
-            board[headR][headC] = 1;
-        }
-        else {
-            board[headR][headC] = 1;
-            int tailR = snake.front().first;
-            int tailC = snake.front().second;
-            snake.pop();
-            board[tailR][tailC] = 0;
-        }
-    }
-    cout << now << endl;
-    
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    solve();
     return 0;
 }
